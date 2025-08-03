f0250ff8b285b47937be96dc3823decd179c8c56
LoFiSwanL <daiki.ren710@gmai.com>
2025-08-03 15:26:00 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+// #include <vector>
+
+// int compute() {
+//     int sum = 0;
+//     for (int i = 0; i < 1000000; ++i) {
+//         sum += i % 10;
+//     }
+//     return sum;
+// }
+
+// int main() {
+//     std::vector<int> data(100, 42);
+
+//     int result = compute();
+//     std::cout << "\nResult: " << result << std::endl;
+
+//     return 0;
+// }
