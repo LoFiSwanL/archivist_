affb5fd309390ee97dc9f39a07178194ce91d081
LoFiSwanL <daiki.ren710@gmai.com>
2025-08-28 21:55:52 +0300

    add build system for tree

--- huffman_tree.cpp
+++ huffman_tree.cpp
@@ -7,7 +7,22 @@ bool Compare::operator()(Node *left_, Node *right_)
 
 Node *build_huffman_tree(const std::vector<std::pair<char, int>> &frequencies)
 {
-    
+    std::priority_queue<Node*, std::vector<Node*>, Compare> min_cluster;
+
+    for (auto& p: frequencies){
+        min_cluster.push(new Node(p.first, p.second));
+    }
+
+    while(min_cluster.size() > 1){
+        Node* left = min_cluster.top();
+        min_cluster.pop();
+        Node* right = min_cluster.top();
+        min_cluster.pop();
+
+        Node* parent = new Node('\0', left->frequency + right->frequency, left, right);
+
+        min_cluster.push(parent);
+    }
 }
 
 void generate_code(Node *root, std::vector<bool> way, std::unordered_map<char, std::vector<bool>> &container)
@@ -17,5 +32,5 @@ void generate_code(Node *root, std::vector<bool> way, std::unordered_map<char, s
 
 void delete_all(Node *root)
 {
-    
+
 }
