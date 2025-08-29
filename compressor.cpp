5f90a4f17abe77c80f6aec7c555705041ebe3c5a
LoFiSwanL <daiki.ren710@gmai.com>
2025-08-29 21:22:51 +0300

    update compressor

--- compressor.cpp
+++ compressor.cpp
@@ -1,2 +1,23 @@
 #include "compressor.h"
 #include "huffman_tree.h"
+#include <cstdint>
+
+using namespace std;
+
+vector<char> binary_read_func(const string &filepath)
+{
+    ifstream file(filepath, ios::binary);
+    if(!file.is_open()){
+        cerr << "could not open file" << endl;
+        return {};
+    }
+    
+    file.seekg(0, ios::end);
+    size_t size = file.tellg();
+    file.seekg(0, ios::beg);
+
+    vector<char> buffer(size);
+    file.read(buffer.data(), size);
+
+    return buffer;
+}
--- compressor.h
+++ compressor.h
@@ -1,2 +1,7 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
+std::vector<char> binary_read_func(const std::string& filepath);
